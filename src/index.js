f184b8df55c4bb530e674e9315f8edfd3bab062b
Louis Cruz <louisstephancruz@me.com>
2019-10-20 23:28:40 -0700

    Remove unnecessary exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { DefaultTimeProviders, generateTimeProviders } from './TimeProviders';
+export { generateTimeProviders } from './TimeProviders';
 export { default as useRelativeTime } from './useRelativeTime';
 export * from './durations';
 export * from './primitives';
