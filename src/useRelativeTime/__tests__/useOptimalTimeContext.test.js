fefcb0011ab28ada0a06e273bcef24715c8ae727
Louis Cruz <louisstephancruz@me.com>
2019-10-27 15:04:16 -0700

    Add tests for useOptimalContext

--- src/useRelativeTime/__tests__/useOptimalTimeContext.test.js
+++ src/useRelativeTime/__tests__/useOptimalTimeContext.test.js
@@ -1,21 +1,186 @@
 import useOptimalTimeContext from '../useOptimalTimeContext';
 import AccuracyMap from '../../classes/AccuracyMap';
 import ONE_SECOND from '../../durations/ONE_SECOND';
+import ONE_MINUTE from '../../durations/ONE_MINUTE';
 
 jest.mock('../../classes/AccuracyMap', () => {
   const ONE_SECOND = require('../../durations/ONE_SECOND').default;
-  class MockAccuracyMap {
-    getOptimalContext() {
-      return ONE_SECOND.context;
-    }
-  }
+
+  class MockAccuracyMap {}
+
+  MockAccuracyMap.prototype.getOptimalContext = jest.fn(() => ONE_SECOND.context);
 
   return MockAccuracyMap;
 });
 
 describe('useOptimalTimeContext()', () => {
-  it('returns the correct duration when passed only a single duration', () => {
-    const context = useOptimalTimeContext([ONE_SECOND], 999, new AccuracyMap(), new AccuracyMap());
-    expect(context).toBe(ONE_SECOND.context);
+  const mockGetOptimalContext = targetDuration => {
+    AccuracyMap.prototype.getOptimalContext.mockImplementationOnce(duration => {
+      if (duration === targetDuration) {
+        return targetDuration.context;
+      } else {
+        throw new Error('getOptimalContext was not passed the correct duration.');
+      }
+    });
+  };
+
+  beforeEach(() => {
+    AccuracyMap.prototype.getOptimalContext.mockClear();
+  });
+
+  describe('when passed a durations list containing a single duration', () => {
+    describe('when the difference is negative', () => {
+      it('returns the correct duration when passed a difference absolutely less than that single duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND],
+          0 - ONE_SECOND.value + 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+
+      it('returns the correct duration when passed a difference absolutely equal to that single duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND],
+          0 - ONE_SECOND.value,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+
+      it('returns the correct duration when passed a difference greater than that single duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND],
+          0 - ONE_SECOND.value - 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+    });
+
+    describe('when the difference is positive', () => {
+      it('returns the correct duration when passed a difference less than that single duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND],
+          ONE_SECOND.value - 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+
+      it('returns the correct duration when passed a difference equal to that single duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND],
+          ONE_SECOND.value,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+
+      it('returns the correct duration when passed a difference greater than that single duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND],
+          ONE_SECOND.value + 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+    });
+  });
+
+  describe('when passed a durations list containing multiple durations', () => {
+    describe('when the difference is negative', () => {
+      it('returns the correct duration when passed a difference absolutely less than the target duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND, ONE_MINUTE],
+          0 - ONE_MINUTE.value + 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+
+      it('returns the correct duration when passed a difference absolutely equal to the target duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND, ONE_MINUTE],
+          0 - ONE_MINUTE.value,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+
+      it('returns the correct duration when passed a difference greater than the target duration', () => {
+        mockGetOptimalContext(ONE_MINUTE);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND, ONE_MINUTE],
+          0 - ONE_MINUTE.value - 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_MINUTE.context);
+      });
+    });
+
+    describe('when the difference is positive', () => {
+      it('returns the correct duration when passed a difference less than the target duration', () => {
+        mockGetOptimalContext(ONE_SECOND);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND, ONE_MINUTE],
+          ONE_MINUTE.value - 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_SECOND.context);
+      });
+
+      it('returns the correct duration when passed a difference equal to the target duration', () => {
+        mockGetOptimalContext(ONE_MINUTE);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND, ONE_MINUTE],
+          ONE_MINUTE.value,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_MINUTE.context);
+      });
+
+      it('returns the correct duration when passed a difference greater than the target duration', () => {
+        mockGetOptimalContext(ONE_MINUTE);
+
+        const context = useOptimalTimeContext(
+          [ONE_SECOND, ONE_MINUTE],
+          ONE_MINUTE.value + 1,
+          new AccuracyMap(),
+          new AccuracyMap()
+        );
+        expect(context).toBe(ONE_MINUTE.context);
+      });
+    });
   });
 });
--- src/useRelativeTime/useOptimalTimeContext.js
+++ src/useRelativeTime/useOptimalTimeContext.js
@@ -2,22 +2,26 @@ const isAbsolutelyBetweenDurations = (
   difference,
   absoluteDifference,
   upperBound = Number.POSITIVE_INFINITY,
-  lowerBound = Number.NEGATIVE_INFINITY
+  lowerBound
 ) =>
   (difference >= 0 && difference >= lowerBound && difference < upperBound) ||
   (difference <= 0 && absoluteDifference > lowerBound && absoluteDifference <= upperBound);
 
+// This function assumes that durations is an array with at least one duration.
+// That's a fair assumption given that TimeProviders must have at least one
+// duration.
 const findCorrectDuration = (durations, difference, absoluteDifference) => {
   for (let i = 1; i <= durations.length; i++) {
-    const lesserDuration = durations[i - 1] || {};
+    const lesserDuration = durations[i - 1];
     const greaterDuration = durations[i] || {};
 
-    const isCorrectDuration = isAbsolutelyBetweenDurations(
-      difference,
-      absoluteDifference,
-      greaterDuration && greaterDuration.value,
-      lesserDuration && lesserDuration.value
-    );
+    const isCorrectDuration =
+      isAbsolutelyBetweenDurations(
+        difference,
+        absoluteDifference,
+        greaterDuration && greaterDuration.value,
+        lesserDuration && lesserDuration.value
+      ) || !durations[i];
 
     if (isCorrectDuration) {
       return lesserDuration;
