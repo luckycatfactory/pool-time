ff63af1d1b7efc2fff5a5dfaa9b2e0e546684879
Louis Cruz <louisstephancruz@me.com>
2020-05-30 18:24:02 -0700

    fix: fix eslint in cli.js

--- cli.js
+++ cli.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-const path = require('path');
+const path = require('path'); // eslint-disable-line @typescript-eslint/no-var-requires
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
 
 bootstrap({
   cliPath: path.join(__dirname, '../node_modules/commitizen'),
-  // this is new
   config: {
     path: 'cz-conventional-changelog',
   },
